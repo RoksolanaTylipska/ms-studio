c3c26b010269df53303bd1fcf7289ab194f5fda1
RossRodman <ross55@gmail.com>
2025-02-10 00:57:36 +0200

    fix

--- src/constants/eyebrow.ts
+++ src/constants/eyebrow.ts
@@ -4,39 +4,51 @@ export const eyebrow: Zone[] = [
   {
     title: "face.eyebrow",
     variants: [
-      { label: "face.eyebrow_correction", price: [250] },
-      { label: "face.tinting", price: [250] },
-      { label: "face.correction_tinting", price: [350] },
+      { label: "face.eyebrow_correction", price: [300] },
+      { label: "face.tinting", price: [350] },
+      { label: "face.correction_tinting", price: [500] },
     ],
   },
   {
     title: "face.lamination",
     variants: [
-      { label: "face.lamination_tinting", price: [750] },
-      { label: "face.lasting_styling", price: [450, 550] },
+      { label: "face.lamination_tinting", price: [900] },
+      { label: "face.lasting_styling", price: [500] },
+      { label: "lasting_styling_correction", price: [550] },
+      { label: "lasting_styling_correction_tinting", price: [650] },
+
     ],
   },
-  {
-    title: "face.makeup",
-    variants: [
-      { label: "face.day", price: [600] },
-      { label: "face.evening", price: [1000] },
-      { label: "face.express", price: [500] },
-    ],
-  },
+  // {
+  //   title: "face.makeup",
+  //   variants: [
+  //     { label: "face.day", price: [600] },
+  //     { label: "face.evening", price: [1000] },
+  //     { label: "face.express", price: [500] },
+  //   ],
+  // },
   {
     title: "face.complexes",
     variants: [
-      { label: "face.lamination_eyebrows", price: [1100] },
-      { label: "face.eyebrow_eyelash_lamination", price: [1250] },
-      { label: "face.eyebrow_eyelash_lamination_without", price: [1150] },
+      { label: "face.lamination_eyebrows", price: [1300] },
+      { label: "face.lamination_eyebrows_correction", price: [1400] },
+      { label: "face.lamination_eyebrows_correction_tinting", price: [1500] },
+      { label: "face.lamination_eyebrows_without", price: [1350] },
+
+    ],
+  },
+  {
+    title: "face.man_eyebrows",
+    variants: [
+      { label: "face.man_correction", price: [400] },
+      { label: "face.man_correction_thin", price: [500] },
     ],
   },
   {
     title: "face.permanent",
     variants: [
-      { label: "face.initial_procedure", price: [2500] },
-      { label: "face.correction", price: [1500] },
+      { label: "face.initial_procedure", price: [3000] },
+      { label: "face.correction", price: [1700] },
     ],
   },
 ];
