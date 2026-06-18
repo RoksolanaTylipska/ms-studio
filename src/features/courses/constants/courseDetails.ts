8c1a440f4bbbdbb4e5318d9c26fcef19655e9bfc
roksolana <roksolana1407@gmail.com>
2026-06-18 09:55:43 +0300

    fix: price cource

--- src/features/courses/constants/courseDetails.ts
+++ src/features/courses/constants/courseDetails.ts
@@ -113,6 +113,7 @@ export const courseDetails: Course[] = [
       title: "courses.electroepilation_qualification.diploma.title",
       description: "courses.electroepilation_qualification.diploma.topics",
     },
+    price: "courses.electroepilation_qualification.price",
   },
   {
     link: "online-consultation",
